de0b65c7dd0647e27346af95604bd9199825b506
Matvey Lazarev <matv26-01@mail.ru>
2023-08-28 18:04:03 +0300

    Change

--- src/Main5.java
+++ src/Main5.java
@@ -4,3 +4,10 @@ public class Main5{
         
     }
 }
+
+// 1) Task (#1253)
+// 2) git clone repo (master/main, feature...)
+// 3) git checkout -b feature/task_name
+// 4) разработка
+// 5) git push origin feature/task_name
+// 6)
